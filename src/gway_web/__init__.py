02f76ad7c1146275aa912089253bce29beeac915
arthexis <arthexis@gmail.com>
2026-09-09 06:21:05 -0600

    feat: expose site primitives

--- src/gway_web/__init__.py
+++ src/gway_web/__init__.py
@@ -1,5 +1,19 @@
 """Portable web deployment capabilities for GWAY."""
 
-__all__ = ["__version__"]
+from .registry import clear, get, load, register, sites, unregister
+from .site import Site, upstream_url, url
+
+__all__ = [
+    "Site",
+    "__version__",
+    "clear",
+    "get",
+    "load",
+    "register",
+    "sites",
+    "unregister",
+    "upstream_url",
+    "url",
+]
 
 __version__ = "0.1.0"
