c35f5e561b1c9b9987481e3438ab2ff407b3dc13
arthexis <arthexis@gmail.com>
2026-09-15 17:06:10 -0600

    Fix API runtime dependency assertion

--- tests/test_api_service.py
+++ tests/test_api_service.py
@@ -20,4 +20,4 @@ def test_managed_api_rejects_non_loopback_bindings(host):
 def test_managed_api_runtime_includes_gway_dispatcher():
     from gway.dispatcher import Dispatcher
 
-    assert Dispatcher.__module__ == "gway.dispatcher"
+    assert Dispatcher.__name__ == "Dispatcher"
